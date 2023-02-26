4b92db3dc993dc0575baa9f30d6ea19ec3a3bb05
Откисян <77882873+otkisyan@users.noreply.github.com>
2023-02-26 20:11:49 +0100

    tmux.sh update

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -2,6 +2,7 @@
 
 cp -f .tmux.conf ~/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux new-session -d -s mysession
+tmux new-session -d -s setupsession
 tmux source-file ~/.tmux.conf
+tmux kill-session -t setupsession
 
