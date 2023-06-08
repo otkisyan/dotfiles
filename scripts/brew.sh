8e32d41cadfc6059aa4e898ff433e02111310d1e
Откисян <77882873+otkisyan@users.noreply.github.com>
2023-06-08 19:47:28 +0200

    scripts and readme edit

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -2,14 +2,10 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew update
-
 brew install git
 brew install node
 brew install --cask iterm2
 brew install neovim
 brew install exa
-brew tap shaunsingh/SFMono-Nerd-Font-Ligaturized
-brew install --cask font-sf-mono-nerd-font-ligaturized
 brew install tmux
 brew install ripgrep
